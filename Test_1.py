bac3b1ed8a360a2789efae8962216bd7a45897f9
Sagar Jain <48284982+sagarjain007@users.noreply.github.com>
2024-06-20 11:45:48 +0530

    updated random function

--- Test_1.py
+++ Test_1.py
@@ -1,7 +1,7 @@
 import random
 
-# Generate a random number between 1 and 100
-random_number = random.randint(1, 1000)
+# Generate a random integer number between 1 and 100
+random_number = random.uniform(1, 1000)
 
 # Print the random number
 print(f"The random number is: {random_number}")
